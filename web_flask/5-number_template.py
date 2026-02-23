fcc6793de41b8d64000cf0d1b362465ad8b57ae4
shakilla1 <s.uwamahoro1@alustudent.com>
2026-02-23 07:43:54 -0800

    add my file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -25,6 +25,10 @@ def python_text(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return f"{n} is a number"
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
